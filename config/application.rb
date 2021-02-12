5334adb3e9e385c69e3bfc7dc52efc46bf5e69c6
takahirohirayama <honobonohinatabokko@gmail.com>
2021-02-12 17:01:02 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
